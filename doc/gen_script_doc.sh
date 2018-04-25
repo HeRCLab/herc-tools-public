395e1d577cc3390e685e128e198868e38d4e82fa
Charles Daniels <cdaniels@fastail.com>
2018-04-25 18:15:05 -0400

    switch generator script from wget to curl

--- doc/gen_script_doc.sh
+++ doc/gen_script_doc.sh
@@ -5,7 +5,7 @@ cd "$(dirname "$0")/.."
 SHELLDOC_URL='https://raw.githubusercontent.com/charlesdaniels/shelldoc/master/shelldoc'
 
 if [ ! -x "$(which shelldoc)" ] ; then
-	wget "$SHELLDOC_URL" -o ./shelldoc
+	curl -LSs "$SHELLDOC_URL" -o ./shelldoc
 	chmod +x shelldoc
 	SHELLDOC_CMD="./shelldoc"
 else
